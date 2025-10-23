9a87d5f1cd656ce2a36f37358f5a3d61f6d79b7f
Elisardo Felix <elisardofelix@gmail.com>
2025-10-23 00:07:13 +0000

    added last dependencies on script

--- install_protoc.sh
+++ install_protoc.sh
@@ -16,4 +16,11 @@ sudo apt install -y protobuf-compiler
 # Install Go protobuf plugins
 # -----------------------------------------
 echo "Installing Go protobuf plugins..."
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
+# Add Go bin to PATH if not already
+if [[ ":$PATH:" != *":$HOME/go/bin:"* ]]; then
+  echo "export PATH=\$PATH:\$HOME/go/bin" >> ~/.bashrc
+  export PATH=$PATH:$HOME/go/bin
+fi
