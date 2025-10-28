1d93cff6c3b5aee741c8cae187128fc2a0f17637
Elisardo Felix <elisardofelix@gmail.com>
2025-10-28 02:33:25 +0000

    initial example 3 for straming on grpc

---
+++ example-3/internal/streaming/service.go
@@ -0,0 +1,41 @@
+package streaming
+
+import (
+	"time"
+
+	"github.com/elisardofelix/grpc-examples/example-3/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type Service struct {
+	proto.UnimplementedStreamingServiceServer
+}
+
+func (s Service) StreamServerTime(request *proto.StreamServerTimeRequest, stream proto.StreamingService_StreamServerTimeServer) error {
+	if request.GetIntervalSeconds() == 0 {
+		return status.Error(codes.InvalidArgument, "interval must be set")
+	}
+
+	interval := time.Duration(request.GetIntervalSeconds()) * time.Second
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-stream.Context().Done():
+			return nil
+		case <-ticker.C:
+			currentTime := time.Now()
+
+			resp := &proto.StreamServerTimeResponse{
+				CurrentTime: timestamppb.New(currentTime),
+			}
+
+			if err := stream.Send(resp); err != nil {
+				return err
+			}
+		}
+	}
+}
